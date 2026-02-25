26a7df5cc7e73b1d6fdca99670158fae2c0dcb9d
shreedharSRG <shreegavali2@gmail.com>
2026-02-25 23:09:45 +0530

    Add flex inside form

--- src/App.js
+++ src/App.js
@@ -7,16 +7,22 @@ import Welcome from './components/classComponent';
 import Name from './components/useProps';
 import Text from './components/usestate';
 import Counter from './components/setCounter';
+import Form from './components/form';
+import Hookcounter from './components/hookCounter';
+import form from './components/form';
 function App() {
   return (
     
     <div class="App">
-      <Greet  />                                        
+      {/* <Greet  />                                        
       <MyComponent />
       <Welcome />
       <Name fname="krishna"  lname="Gavali"> <p>children props</p> </Name>
        <Text/>
        <Counter />
+      <Hookcounter /> */}
+      <Form/>
+
     </div>
   );
 }
--- src/components/form.js
+++ src/components/form.js
@@ -7,22 +7,22 @@ function Form(){
      alignItems:"center" , gap:"10px",padding:"10px" , 
       backgroundColor:"powderblue" , marginBottom: "10px"};
 
-   return( <div> 
-    <div style={{display:"flex", padding:"10px", }}>
-    <div className="Name" style={rowStyle}>
+   return( <div style={rowStyle}> 
+
+<div className="Name" style={{ padding:"10px", margin:"10px",padding:"20px",backgroundColor:"#f1f1f1" }}>
     <label htmlFor="name">Name:</label>
-<input type="text" id="name" value={name} onChange={(e)=>setName(e.target.value)}></input>
+    <input type="text" id="name" value={name} onChange={(e)=>setName(e.target.value)}></input>
 </div>
-<div className="Email" style={rowStyle}>
-<label htmlFor="e-mail">E-mail</label>
-<input type="email" id="e-mail" value={email} onChange={(e)=>setEmail(e.target.value)}></input>
-</div>
-</div>
-<div >
-    <label style={rowStyle}>Gender:</label>
+<div className="Email" style={{padding:"10px", margin:"10px",padding:"20px",backgroundColor:"#f1f1f1"}}>
+    <label htmlFor="e-mail">E-mail</label>
+    <input type="email" id="e-mail" value={email} onChange={(e)=>setEmail(e.target.value)}></input>
 </div>
 
-< div className="genderCategary"style={rowStyle}>
+<div style={{}} >
+    <label >Gender:</label>
+</div>
+
+< div className="genderCategary"style={{padding:"10px", margin:"10px",padding:"20px",backgroundColor:"#f1f1f1"}}>
 <label htmlFor="male">Male</label>
 <input id="male" type="radio" name="gender"></input>
 <label htmlFor="female">Female</label>
